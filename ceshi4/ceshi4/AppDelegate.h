fb04b1287b176470cdaad1a3e29764c54c668620
hu胡洁佩 <hjp@BETTERME.local>
2016-04-18 22:11:48 +0800

    项目4

---
+++ ceshi4/ceshi4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi4
+//
+//  Created by hu胡洁佩 on 16/4/18.
+//  Copyright © 2016年 胡洁佩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshi4/ceshi4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshi4
+//
+//  Created by hu胡洁佩 on 16/4/18.
+//  Copyright © 2016年 胡洁佩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
